0c4a30ba2e58d6b1bec84159019276921c12ed65
desrtCodr <ian.mitchard@gmail.com>
2023-01-02 16:34:19 -0700

    converted battle to hooks

--- pages/battle.js
+++ pages/battle.js
@@ -15,47 +15,41 @@ function Instuctions() {
     </section>
   );
 }
+function PlayerInput({ label, onSubmit }) {
+  const [username, setUsername] = React.useState('');
 
-class PlayerInput extends React.Component {
-  state = {
-    username: '',
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    onSubmit(username);
   };
-  handleSubmit = (event) => {
-    event.preventDefault();
 
-    this.props.onSubmit(this.state.username);
+  const handleChange = (e) => {
+    setUsername(e.target.value);
   };
-  handleChange = (event) => {
-    this.setState({
-      username: event.target.value,
-    });
-  };
-  render() {
-    return (
-      <form className='card' onSubmit={this.handleSubmit}>
-        <label htmlFor='username' className='player-label'>
-          {this.props.label}
-        </label>
-        <div className='input-row'>
-          <input
-            type='text'
-            id='username'
-            placeholder='github username'
-            autoComplete='off'
-            value={this.state.username}
-            onChange={this.handleChange}
-          />
-          <button
-            className='btn link'
-            type='submit'
-            disabled={!this.state.username}
-          >
-            Submit
-          </button>
-        </div>
-      </form>
-    );
-  }
+  return (
+    <form className='card' onSubmit={handleSubmit}>
+      <label htmlFor='username' className='player-label'>
+        {label}
+      </label>
+      <div className='input-row'>
+        <input
+          type='text'
+          id='username'
+          placeholder='github username'
+          autoComplete='off'
+          value={username}
+          onChange={handleChange}
+        />
+        <button
+          className='btn link'
+          type='submit'
+          disabled={!username}
+        >
+          Submit
+        </button>
+      </div>
+    </form>
+  );
 }
 
 function PlayerPreview({ username, onReset, label }) {
@@ -85,73 +79,59 @@ PlayerPreview.propTypes = {
   onReset: PropTypes.func.isRequired,
   label: PropTypes.string.isRequired,
 };
+export default function Battle() {
+  const [playerOne, setPlayerOne] = React.useState(null);
+  const [playerTwo, setPlayerTwo] = React.useState(null);
+  const disabled = !playerOne || !playerTwo;
 
-export default class Battle extends React.Component {
-  state = {
-    playerOne: null,
-    playerTwo: null,
+  const handleSubmit = (id, player) => {
+    id === 'playerOne' ? setPlayerOne(player) : setPlayerTwo(player);
   };
 
-  handleSubmit = (id, player) => {
-    this.setState({
-      [id]: player,
-    });
+  const handleReset = (id) => {
+    id === 'playerOne' ? setPlayerOne(null) : setPlayerTwo(null);
   };
-  handleReset = (id) => {
-    this.setState({
-      [id]: null,
-    });
-  };
-  render() {
-    const { playerOne, playerTwo } = this.state;
-    const disabled = !playerOne || !playerTwo;
-
-    return (
-      <main className='stack main-stack animate-in'>
-        <div className='split'>
-          <h1>Players</h1>
-          <Link
-            href={{
-              pathname: '/results',
-              query: `playerOne=${playerOne}&playerTwo=${playerTwo}`,
-            }}
-            className={`btn primary ${disabled ? 'disabled' : ''}`}
-          >
-            Battle
-          </Link>
-        </div>
-        <section className='grid'>
-          {playerOne === null ? (
-            <PlayerInput
-              label='Player One'
-              onSubmit={(player) =>
-                this.handleSubmit('playerOne', player)
-              }
-            />
-          ) : (
-            <PlayerPreview
-              label='Player One'
-              username={playerOne}
-              onReset={() => this.handleReset('playerOne')}
-            />
-          )}
-          {playerTwo === null ? (
-            <PlayerInput
-              label='Player Two'
-              onSubmit={(player) =>
-                this.handleSubmit('playerTwo', player)
-              }
-            />
-          ) : (
-            <PlayerPreview
-              label='Player Two'
-              username={playerTwo}
-              onReset={() => this.handleReset('playerTwo')}
-            />
-          )}
-        </section>
-        <Instuctions />
-      </main>
-    );
-  }
+  return (
+    <main className='stack main-stack animate-in'>
+      <div className='split'>
+        <h1>Players</h1>
+        <Link
+          href={{
+            pathname: '/results',
+            query: `playerOne=${playerOne}&playerTwo=${playerTwo}`,
+          }}
+          className={`btn primary ${disabled ? 'disabled' : ''}`}
+        >
+          Battle
+        </Link>
+      </div>
+      <section className='grid'>
+        {playerOne === null ? (
+          <PlayerInput
+            label='Player One'
+            onSubmit={(player) => handleSubmit('playerOne', player)}
+          />
+        ) : (
+          <PlayerPreview
+            label='Player One'
+            username={playerOne}
+            onReset={() => handleReset('playerOne')}
+          />
+        )}
+        {playerTwo === null ? (
+          <PlayerInput
+            label='Player Two'
+            onSubmit={(player) => handleSubmit('playerTwo', player)}
+          />
+        ) : (
+          <PlayerPreview
+            label='Player Two'
+            username={playerTwo}
+            onReset={() => handleReset('playerTwo')}
+          />
+        )}
+      </section>
+      <Instuctions />
+    </main>
+  );
 }
