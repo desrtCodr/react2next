4727b6a15965107f75a1f3ee14366b4fb1473d67
Nathan Pertuset <nathanpertuset@gmail.com>
2023-01-03 20:27:48 -0400

    Remove react fragment on return

--- components/withSearchParams.js
+++ components/withSearchParams.js
@@ -8,9 +8,7 @@ export default function withSearchParams(Component) {
     const query = router.query;
 
     return (
-      <>
         <Component {...props} router={query} />
-      </>
     );
   };
 }
